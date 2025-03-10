09be38bedc27bf6c3eb972f07c3a898a701a3744
USPMenas <81880525+USPMenas@users.noreply.github.com>
2025-03-10 09:15:18 +0000

    :sparkles: generic-counter

--- generic-counter/src/main.rs
+++ generic-counter/src/main.rs
@@ -1,11 +1,12 @@
 use std::collections::HashMap;
+use std::hash::Hash;
 
 /// Counter counts the number of times each value of type `T` has been seen.
-struct Counter {
-    values: HashMap<u32, u64>,
+struct Counter<T> {
+    values: HashMap<T, u64>,
 }
 
-impl Counter {
+impl<T: Eq + Hash> Counter<T> {
     /// Create a new Counter.
     fn new() -> Self {
         Counter {
@@ -14,17 +15,13 @@ impl Counter {
     }
 
     /// Count an occurrence of the given value.
-    fn count(&mut self, value: u32) {
-        if self.values.contains_key(&value) {
-            *self.values.get_mut(&value).unwrap() += 1;
-        } else {
-            self.values.insert(value, 1);
-        }
+    fn count(&mut self, value: T) {
+        *self.values.entry(value).or_insert(0) += 1;
     }
 
     /// Return the number of times the given value has been seen.
-    fn times_seen(&self, value: u32) -> u64 {
-        self.values.get(&value).copied().unwrap_or_default()
+    fn times_seen(&self, value: &T) -> u64 {
+        self.values.get(value).copied().unwrap_or_default()
     }
 }
 
@@ -38,12 +35,12 @@ fn main() {
     ctr.count(11);
 
     for i in 10..20 {
-        println!("saw {} values equal to {}", ctr.times_seen(i), i);
+        println!("saw {} values equal to {}", ctr.times_seen(&i), i);
     }
 
     let mut strctr = Counter::new();
     strctr.count("apple");
     strctr.count("orange");
     strctr.count("apple");
-    println!("got {} apples", strctr.times_seen("apple"));
+    println!("got {} apples", strctr.times_seen(&"apple"));
 }
